44c47f615d2b3b01a4e1dfa3270cf40637d6009b
tkelamis <kelamis62@gmail .com>
2025-03-13 16:48:30 +0200

    added lazy load in routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,9 @@ import { AboutComponent } from './footer/about/about.component';
 
 export const routes: Routes = [
     {path: '', component: TaskTableComponent},
+
+    //EagerLoading
     {path: 'about', component: AboutComponent}
+    // LazyLoading
+    //{path: 'about', loadComponent:() => import("../app/footer/about/about.component").then(c => c.AboutComponent)}
 ];
