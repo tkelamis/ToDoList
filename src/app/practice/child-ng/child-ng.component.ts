388964ebf263d230ae5b0cb3e38b0c9a66fffb8d
tkelamis <kelamis.anastasios@gmail.com>
2025-04-14 18:05:36 +0300

    added ng template blocks with ngIf then

--- src/app/practice/child-ng/child-ng.component.ts
+++ src/app/practice/child-ng/child-ng.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { NgTemplateOutlet } from '@angular/common';
+import { Component, Input, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-child-ng',
-  imports: [],
+  imports: [NgTemplateOutlet ],
   templateUrl: './child-ng.component.html',
   styleUrl: './child-ng.component.css'
 })
 export class ChildNgComponent {
+  @Input() receivedDivFromParent!: TemplateRef<any>
 
 }
--- src/app/practice/parent-ng/parent-ng.component.ts
+++ src/app/practice/parent-ng/parent-ng.component.ts
@@ -1,11 +1,73 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { ChildNgComponent } from "../child-ng/child-ng.component";
 
 @Component({
   selector: 'app-parent-ng',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './parent-ng.component.html',
   styleUrl: './parent-ng.component.css'
 })
-export class ParentNgComponent {
 
+export class ParentNgComponent implements OnInit {
+  
+  thenBlock: TemplateRef<any>| null = null;
+  show = true;
+
+  showChildComponent: boolean = true;
+  @ViewChild('primaryBlock', {static: true}) primaryBlock: TemplateRef<any>| null = null;
+  @ViewChild('secondaryBlock', {static: true}) secondaryBlock: TemplateRef<any>| null = null;
+
+
+  switchPrimary() {
+    this.thenBlock = this.thenBlock === this.primaryBlock ? this.secondaryBlock : this.primaryBlock;
+  }
+
+  ngOnInit(): void {
+    this.thenBlock = this.primaryBlock;
+    }
 }
+  
+
+//   async getTodos(): Promise<string[]> {
+//     const todos = ['Buy milk', 'Study Angular', 'Walk dog'];
+//     return todos;
+//   }
+
+//   async showTodos() {
+//     const todos: Todos = await this.getTodos();
+//     console.log(todos); // ['Buy milk', 'Study Angular', 'Walk dog']
+//   }
+// }
+// type Todos = Awaited<ReturnType<typeof ParentNgComponent.prototype.getTodos>>;
+
+
+  
+
+
+//   toggleFlag() {
+//     if(this.showChildComponent === true){
+//       this.showChildComponent = false;
+//     }
+//     else{
+//       this.showChildComponent = true;
+//     }
+//   }
+
+//   showValue() {
+    
+//     let variable = 1;
+    
+//     if(variable==1){
+//       let variable = 2;
+//       console.log(variable)
+//     }
+
+//     console.log(variable)
+
+//     const message = `This is
+// a multi-line 
+// string.`;
+
+// console.log(message);
+//   }
